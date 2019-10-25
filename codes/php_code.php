cabba886fd6a82c45e1c2df8b5a07178e901f6a4
016 <boy.lee.here@gmail.com>
2019-10-25 20:05:14 +0800

    Create php_code.php
    add php test code

---
+++ codes/php_code.php
@@ -0,0 +1,55 @@
+	public function actionDbTest(){
+
+	    $maxRun = 50000;
+
+        echo "PHP CLI Db Test $maxRun times! \n\n";
+        
+        $startTime = microtime(true);
+        
+        
+        $sum = 0;
+        $cnt = 0;
+        
+        for ($i = 0; $i < $maxRun; $i++){
+            $tmpId = mt_rand(1, 30);
+            
+//             echo "loading.... $tmpId \n";
+            //AR
+//             $l = AppLocation::find()->where(['l_id'=>$tmpId])->one();
+
+            //DAO
+            $sql = 'select * from app_location where l_id = :l_id';
+            $CMD = \Yii::$app->db->createCommand($sql);
+            $CMD->bindValue(':l_id', $tmpId);
+            
+            $l = $CMD->queryOne();
+            
+            //force fake loop
+            for ($i = 0; $i < 10; $i++) {
+                if ($i%2 == 0) {
+                    $sum += $l['l_id'];
+                }else{
+                    $sum += $l['l_code'];
+                }
+            }
+            
+            $cnt ++;
+        }
+        
+        
+        
+        $endTime = microtime(true);
+        echo ">> start $startTime \n";
+        echo ">> end $endTime \n";
+        
+        $diff = $endTime - $startTime;
+        
+        
+        echo "use time : $diff sum: $sum count:$cnt \n\n";
+        
+        
+        
+        
+        
+        
+    }
